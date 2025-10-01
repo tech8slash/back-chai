9bc797082f35c276842aa321c4e4df2fb02e066e
tech8slash <tech08slash@gmail.com>
2025-10-01 13:08:48 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv";
 import connectDb from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
     path: './env'
 });
 
 connectDb()
+.then(() => {
+    app.listen(process.env.PORT  || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+})
+})
+.catch((err) => {
+    console.log("Database connection failed");
+    console.log(err);
+});
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse {
+    constructor(statusCode, message="success", data) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.success = stausCode < 400;
+        this.data = data;
+    }
+}
