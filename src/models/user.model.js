669a61c7f07879ccb4e800eae480e86429868ff4
tech8slash <tech08slash@gmail.com>
2025-10-02 13:34:26 +0530

    error fixed(module not found)

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,7 +1,7 @@
 import mongoose,{Schema} from 'mongoose';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { use } from 'react';
+
 
 const userSchema = new Schema({
     username: {
